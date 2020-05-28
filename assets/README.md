52579cf31a0e141458475e2887eed5de9e8108f7
Haven <stackandslash@gmail.com>
2020-05-27 17:39:17 -0700

    Basic framework of prompts and printout complete, more complex markup and external calls to come.

---
+++ assets/README.md
@@ -0,0 +1,33 @@
+
+# b
+        
+### Description
+b
+        
+## Table of Contents
+#### Installation
+#### Usage
+#### License
+#### Contributing
+#### Tests
+#### Questions
+
+### Installation
+b
+
+### Usage
+b
+
+### License
+b
+
+### Contributing
+b
+
+### Tests
+b
+
+### Questions
+
+* Github Profile Pic
+* Github email
--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const axios = require("axios");
 const inquirer = require("inquirer");
-
+const fs = require("fs");
 const questions = [
     {
-        name: "GitHubName",
+        name: "githubname",
         message: "Enter your GitHub Username"
-    }
+    },
+    {
+        name: "title",
+        message: "Enter the title of your project"
+    },
+    {
+        name: "description",
+        message: "Enter a basic description of your project"
+    },
+    {
+        name: "install",
+        message: "Enter instructions to install your project"
+    },
+    {
+        name: "usage",
+        message: "Enter instructions to use your project"
+    },
+    {
+        name: "license",
+        message: "Enter licensing information for your project"
+    },
+    {
+        name: "contributing",
+        message: "Enter contribution credits for your project"
+    },
+    {
+        name: "tests",
+        message: "Enter tests for your project"
+    },
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+        console.log("Wow! It's Made!");
+      });
 }
 
 function init() {
@@ -18,8 +53,42 @@ function init() {
 init();
 
 function questionAsk(){
-    for (let i = 0; i < questions.length; i++) {
-        inquirer.prompt(questions[i]);
-    }
+    inquirer.prompt(questions).then((answers) => {
+        const {githubname, title, description, install, usage, license, contributing, tests} = answers;
+        console.log(githubname);
+        let markdown = `
+# ${title}
+        
+## Description
+${description}
+        
+## Table of Contents
+### Installation
+### Usage
+### License
+### Contributing
+### Tests
+### Questions
 
+## Installation
+${install}
+
+## Usage
+${usage}
+
+## License
+${license}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+
+### Questions
+
+* Github Profile Pic
+* Github email`;
+        writeToFile("./assets/README.md", markdown);
+    });
 }
